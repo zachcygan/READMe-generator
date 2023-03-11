b70a856d5b7568b3193db4e4f59beb2ad3dc2275
Zachary Cygan <zachcygan@gmail.com>
2023-03-10 20:42:31 -0800

    removed comments

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -53,7 +51,7 @@ const questions = [
     }
 ];
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fileName = data.projectName;
     
@@ -130,9 +128,6 @@ If you have any questions, please contact me at: ${data.email}
     );
 }
 
-
-
-// TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
         .then((data) => {
@@ -141,6 +136,6 @@ function init() {
 
 }
 
-// Function call to initialize app
+
 init();
 
